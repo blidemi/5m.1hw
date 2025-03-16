29490889734ce7dc362d2ee93cbaec33c53e208f
Ademi <ademiergasheva@gmail.com>
2025-03-16 14:39:29 +0600

    Update Page2.jsx

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,39 +1,23 @@
-// import React from 'react';
-
-// const Page2 = () => {
-//     return (
-//         <div>
-//             <h1>Page 2</h1>
-//         </div>
-//     );
-// }
-
-// export default Page2;
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
+const Page2 = () => {
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
 
-const Page1 = () => {
-    const [users, setUsers]= useState([]);
-    const navigate = useNavigate();
-        useEffect(()=>{
-            axios('https://jsonplaceholder.typicode.com/users')
-            .then(({data})=>setUsers(data))
-        },[])
-    return (
-        <div className="container">
-            <h1>Page1</h1>
-            <button className="btn" onClick={()=>{
-            navigate(-1)
-           }}>Go back</button>
-            {
-                users.map(item=>{
-                    return <h1>{item.name}</h1>
-                })
-            }
-        </div>
-    );
+  useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/posts')
+      .then(({ data }) => setPosts(data.slice(0, 5)));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Page 2</h1>
+      <button className="btn" onClick={() => navigate(-1)}>Go back</button>
+      {posts.map(item => <h1 key={item.id}>{item.title}</h1>)}
+    </div>
+  );
 }
 
-export default Page1;
+export default Page2;
